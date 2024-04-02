a1d198c9ed0f91f5af3fc9fb2ef9a6f1e45bdb74
bookworm2035 <bookworm2035@gmail.com>
2024-04-02 15:02:19 -0600

    Add project files.

---
+++ 414Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _414Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
